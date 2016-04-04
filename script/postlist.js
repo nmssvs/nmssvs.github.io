be9c74898067db28f3d8d64604e1d9b1a88a067d
Verna Koskinen <darth.kipsu@gmail.com>
2016-04-04 22:16:50 +0300

    refactor postlist.js

--- script/postlist.js
+++ script/postlist.js
@@ -1,4 +1,7 @@
 
+var MARGIN = 10
+var MAX_WIDTH = 550
+
 function resizeEveryOtherPair(imgContainers, postListWidth) {
     function isBigImage(i) {
         var big = i % 2 == 0 ? i : i + 1;
@@ -9,7 +12,7 @@ function resizeEveryOtherPair(imgContainers, postListWidth) {
         return li.firstElementChild.firstElementChild.firstElementChild
     }
 
-    function imageWidth(i) {
+    function ithImageWidth(i) {
         return imageUnder(imgContainers[i]).offsetWidth
     }
 
@@ -18,31 +21,40 @@ function resizeEveryOtherPair(imgContainers, postListWidth) {
         image.style.margin = cutMargin + ' 0 0 ' + cutMargin
     }
 
-    Array.prototype.forEach.call(imgContainers, function(container, i) {
-        console.log(i)
-        if (isBigImage(i)) {
-            var imgOffsetWidth = imageWidth(i)
-            if (imgOffsetWidth > 550) {
-                cropImage(imageUnder(container), 550)
-                container.style.width = '550px'
-            } else {
-                container.style.width = imgOffsetWidth + 'px'
-            }
-        } else if (i % 2 == 0) {
-            var nextImgWidth = imageWidth(i+1)
-            if (nextImgWidth > 550) {
-                container.style.width = (postListWidth - 550) + 'px'
-            } else {
-                container.style.width = (postListWidth - nextImgWidth) + 'px'
-            }
+    function setContainerWidth(container, width) {
+        container.style.width = width + 'px'
+    }
+
+    function setContainerWidthToImgWidth(container, i) {
+        var imgOffsetWidth = ithImageWidth(i)
+        if (imgOffsetWidth > MAX_WIDTH) {
+            cropImage(imageUnder(container), MAX_WIDTH)
+            setContainerWidth(container, MAX_WIDTH)
         } else {
-            container.style.width = (postListWidth - imageWidth(i-1)) + 'px'
+            setContainerWidth(container, imgOffsetWidth)
+        }
+    }
+
+    function setContainerWidthBasedOnNextImage(container, nextImgWidth) {
+        if (nextImgWidth > MAX_WIDTH) {
+            setContainerWidth(container, postListWidth - MAX_WIDTH)
+        } else {
+            setContainerWidth(container, postListWidth - nextImgWidth)
+        }
+    }
+
+    Array.prototype.forEach.call(imgContainers, function(container, i) {
+        if (isBigImage(i)) {
+            setContainerWidthToImgWidth(container, i)
+        } else if (i % 2 == 0 && i < imgContainers.length - 1) {
+            setContainerWidthBasedOnNextImage(container, ithImageWidth(i+1))
+        } else {
+            setContainerWidth(container, postListWidth - ithImageWidth(i-1))
         }
     })
 }
 
 var postListWidth = document.getElementsByClassName('post-list')[0].offsetWidth
 var imgContainers = document.getElementsByClassName('post-img-container')
-var margin = 10
 
-resizeEveryOtherPair(imgContainers, postListWidth - margin)
+resizeEveryOtherPair(imgContainers, postListWidth - MARGIN)
